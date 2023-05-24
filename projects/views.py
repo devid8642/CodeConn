b64c5ff07385f750872241b5f0db290f6440cf2e
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-05-24 18:47:50 -0300

    home/projects_list_view added

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Project
+
+
+def home(request):
+    week_projects = Project.objects.filter(
+        is_approved=True,
+    )
+
+    return render(
+        request,
+        'users/pages/home.html',
+        context={
+            'week_projects': week_projects,
+        }
+    )
+
+
+def all_projects(request):
+    projects = Project.objects.filter(
+        is_approved=True,
+    ).order_by('-id')
+
+    return render(
+        request,
+        'users/pages/all_projects.html',
+        context={
+            'projects': projects,
+        }
+    )
