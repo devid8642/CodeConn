3e7c442fd03f5cc06b4ec729bfc9808471a389ef
devid <nnick4673@gmail.com>
2023-05-25 14:06:20 -0300

    register and logout views

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', views.loginn, name='login')
+    path('login/', views.loginn, name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', views.logoutt, name='logout')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from . models import User
 
 def loginn(request):
     if request.method == 'GET':
@@ -11,5 +12,40 @@ def loginn(request):
             user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('/')
+                return redirect('projects:home')
+        # Redirect indicating error message
         return render(request, 'auth/pages/login.html')
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'auth/pages/register.html')
+    else:
+        expected_fields = [
+            'username',
+            'email',
+            'password',
+            'confirmed_password'
+        ]
+        for field in expected_fields:
+            if field not in request.POST:
+                # Redirect indicating error message
+                return render(request, 'auth/pages/register.html')
+        if request.POST['password'] != request.POST['confirmed_password']:
+            # Redirect indicating error message
+            return render(request, 'auth/pages/register.html')
+        try:
+            User.objects.get(email = request.POST['email'])
+        except User.DoesNotExist:
+            User.objects.create_user(
+                username=request.POST['username'],
+                email=request.POST['email'],
+                password=request.POST['password']
+            )
+            return redirect('users:login')
+        else:
+            # Redirect indicating error message
+            return render(request, 'auth/pages/register.html')
+
+def logoutt(request):
+    logout(request)
+    return redirect('projects:home')
