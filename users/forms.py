e56bfcf766e3507fe09cdd73dfb158f2500828f3
devid <nnick4673@gmail.com>
2023-05-26 20:32:17 -0300

    new login and register views using forms

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 from .models import User
 
 def add_attr(field, attr, value):
@@ -17,20 +19,15 @@ class LoginForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         add_attr(self.fields['email'], 'placeholder', 'Email')
-        add_attr(self.fields['email'], 'class', 'form-control')
-        add_attr(self.fields['email'], 'autofocus', 'autofocus')
-
         add_attr(self.fields['password'], 'placeholder', 'Digite sua senha')
-        add_attr(self.fields['password'], 'class', 'form-control')
 
 class RegisterForm(forms.Form):
     username = forms.CharField(label='Usuário', max_length=255)
     email = forms.EmailField(label='Email', max_length=255)
     password = forms.CharField(
         label='Senha',
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(),
     )
     confirmed_password = forms.CharField(
         label='Confirme sua senha',
@@ -39,15 +36,29 @@ class RegisterForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        fields = [
-            self.fields['username'],
-            self.fields['email'],
-            self.fields['password'],
-            self.fields['confirmed_password']
-        ]
         add_attr(self.fields['username'], 'placeholder', 'Usuário')
         add_attr(self.fields['email'], 'placeholder', 'Email')
         add_attr(self.fields['password'], 'placeholder', 'Digite uma senha')
         add_attr(self.fields['confirmed_password'], 'placeholder', 'Confirme sua senha')
-        for field in fields:
-            add_attr(field, 'class', 'form-control')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email:
+            exists = User.objects.filter(email=email).exists()
+            if exists:
+                raise ValidationError('Já existe um usuário com este email')
+        return email
+    
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if password:
+            validate_password(password)
+        return password
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirmed_password = cleaned_data.get('confirmed_password')
+        if password and confirmed_password:
+            if password != confirmed_password:
+                raise ValidationError('Você digitou senhas diferentes')
--- users/views.py
+++ users/views.py
@@ -6,12 +6,14 @@ from .forms import LoginForm, RegisterForm
 def login_view(request):
     form = LoginForm(request.POST or None)
     if form.is_valid():
-        email = request.POST.get('email')
-        password = request.POST.get('password')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
         user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
             return redirect('projects:home')
+        else:
+            form.add_error(field=None, error='Email ou senha inválidos')
     return render(
         request,
         'auth/pages/login.html',
@@ -24,20 +26,16 @@ def login_view(request):
 def register_view(request):
     form = RegisterForm(request.POST or None)
     if form.is_valid():
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        confirmed_password = request.POST.get('confirmed_password')
-        if password == confirmed_password:
-            try:
-                User.objects.get(email = email)
-            except User.DoesNotExist:
-                User.objects.create_user(
-                    username=username,
-                    email=email,
-                    password=password
-                )
-                return redirect('users:login')
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        confirmed_password = form.cleaned_data.get('confirmed_password')
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )
+        return redirect('users:login')
     return render(
         request,
         'auth/pages/register.html',
