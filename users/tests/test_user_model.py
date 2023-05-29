01be1870ec494dc7aadacbf0c264f013d2fafd86
devid <nnick4673@gmail.com>
2023-05-29 15:40:04 -0300

    tests for user model

---
+++ users/tests/test_user_model.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.hashers import make_password 
+from utils.tests_bases import ProjectTestBase
+from users.models import User
+
+class TestUserModel(ProjectTestBase):
+    def setUp(self, *args, **kwargs):
+        self.expected_user = self.make_author()
+        return super().setUp(*args, **kwargs)
+
+    def test_user_create(self):
+        user = User.objects.get(id=self.expected_user.id)
+        self.assertEqual(self.expected_user, user)
+
+    def test_user_update(self):
+        self.expected_user.username = 'devid'
+        self.expected_user.email = 'devid@devid.com'
+        self.expected_user.password = make_password('devid3939!')
+        self.expected_user.save(
+            update_fields=[
+                'username',
+                'email',
+                'password'
+            ]
+        )
+        user = User.objects.get(id=self.expected_user.id)
+        self.assertEqual(self.expected_user, user)
+
+    def test_user_delete(self):
+        self.expected_user.delete()
+        exists = User.objects.filter(id=self.expected_user.id).exists()
+        self.assertFalse(exists)
