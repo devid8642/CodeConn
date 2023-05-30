bcbb388c3e59ebdbb9100f1c05e5805740b6a80b
Miguel Almeida <miguel.almeida1009@gmail.com>
2023-05-30 09:51:06 -0300

    project_form placeholders

--- projects/forms.py
+++ projects/forms.py
@@ -1,9 +1,21 @@
 from django.forms import ModelForm
 
 from .models import Project, Comment
+from utils.forms_utils import add_attr
 
 
 class ProjectForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        title = self.fields['title']
+        description = self.fields['description']
+        explanatory = self.fields['explanatory_text']
+
+        add_attr(title, 'placeholder', 'Título do projeto')
+        add_attr(description, 'placeholder', 'Descreva seu projeto')
+        add_attr(explanatory, 'placeholder', 'Sobre o seu projeto')
+
     class Meta:
         model = Project
 
@@ -11,6 +23,11 @@ class ProjectForm(ModelForm):
 
 
 class CommentForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        add_attr(self.fields['comment'], 'placeholder', 'Deixe seu comentário')
+
     class Meta:
         model = Comment
 
---
+++ utils/forms_utils.py
@@ -0,0 +1,10 @@
+def add_attr(field, attr: str, value: str) -> None:
+    '''
+    This function adds a value into a attr in forms.
+
+    Ex: add_attr(email, class, email-field) inject a class
+    email-field into the email field in .html file.
+    '''
+    field.widget.attrs.update(
+        {attr: value}
+    )
