8e43981ef4e8e81099c25170624f29b1a6793604
devid <nnick4673@gmail.com>
2023-06-29 21:34:10 -0300

    changed settings

--- projeto_x/settings.py
+++ projeto_x/settings.py
@@ -194,6 +194,8 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+if prod == 'True':
+    CSRF_COOKIE_DOMAIN = env('DOMAIN')
 
 CKEDITOR_CONFIGS = {
     'default': {
